b8b0bc054a8e3a47aa3742816838acedcab4ae3d
Owidiusz Zieliński <owi19955@gmail.com>
2023-06-03 01:33:21 +0200

    init

---
+++ src/main/java/owi/example/github_restAPI/GithubRestApiApplication.java
@@ -0,0 +1,13 @@
+package owi.example.github_restAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/owi/example/github_restAPI/GithubRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package owi.example.github_restAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
